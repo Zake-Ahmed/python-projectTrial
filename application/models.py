6bf7e4c2598380855c60a8fbee966c34a29f2ebb
Zake-Ahmed <zake3012@gmail.com>
2022-06-19 12:17:09 +0100

    curd copy

---
+++ application/models.py
@@ -0,0 +1,22 @@
+from application import db
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(30))
+    completed = db.Column(db.Boolean, default=False)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(30))
+    firstName = db.Column(db.String(30),nullable = True)
+    lastName = db.Column(db.String(30),nullable = True)
+    #post=db.relationship('Post', backref='users')
+
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message= db.Column(db.String(280),nullable = False)
+    likes = db.Column(db.Integer,nullable = True)
+    userID = db.Column(db.Integer , db.ForeignKey(Users.id), nullable=False)
+ 
