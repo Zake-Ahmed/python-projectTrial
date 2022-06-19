d93ae4be505de6b5d5cf633061216e7a3b895d4f
Zake-Ahmed <zake3012@gmail.com>
2022-06-19 12:41:03 +0100

    update models

--- create.py
+++ create.py
@@ -1,5 +1,5 @@
 from application import db
-from application.models import ToDo
+from application.models import ToDo,Users,Posts
 
 db.drop_all()
 db.create_all()
@@ -7,5 +7,8 @@ sample_todo = ToDo(
     task_name = "Test ToDo",
     completed = False
 )
+rootUser = Users(userName="root",firstName="root",lastName="root")
+rootPost= Posts(massage="root")
+
 db.session.add(sample_todo)
 db.session.commit()
