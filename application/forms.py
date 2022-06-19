77e18734ea85e3047a0f182fb752e7dda97738e3
Zake-Ahmed <zake3012@gmail.com>
2022-06-19 12:34:44 +0100

    forms update

--- application/forms.py
+++ application/forms.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementTree import SubElement
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SelectField, SubmitField
 
@@ -5,3 +6,13 @@ class TaskForm(FlaskForm):
     task_name = StringField("Task")
     completed = BooleanField("Completed", default=False)
     submit = SubmitField("Submit")
+
+class UserForm(FlaskForm):
+    userName= StringField("User Name")
+    firstName = StringField("First Name")
+    lastName = StringField("Last Name")
+    submit= SubmitField("Submit")
+
+class PostForm(FlaskForm):
+    user = SelectField("User Name" ,choices=[])
+    message = StringField("Message")
